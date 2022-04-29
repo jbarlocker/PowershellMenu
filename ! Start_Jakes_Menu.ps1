417d72cfbeb97bb37887cf6ad632abeb3ca1d26c
jbarlocker <jake@barlocker.org>
2022-04-29 14:00:43 -0600

    Add files via upload

--- ! Start_Jakes_Menu.ps1
+++ ! Start_Jakes_Menu.ps1
@@ -8,16 +8,12 @@
 ############################################################
 
 
-
-# Self-elevate the script if required
-    if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-      $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-      Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-      Exit
-     }
-    }
-
+# This is supposed to make Powershell run as an administrator (it will have User Account Control pop-up a window asking for access to be given)
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+}
 
 
 # Check to see if the local computer is a member of a domain.
