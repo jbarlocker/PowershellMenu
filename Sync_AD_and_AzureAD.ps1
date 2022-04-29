c5cb3bc9f7b8c77b5445e7f391e53fe3f2376209
jbarlocker <jake@barlocker.org>
2022-04-29 11:13:33 -0600

    Add files via upload

--- Sync_AD_and_AzureAD.ps1
+++ Sync_AD_and_AzureAD.ps1
@@ -1,9 +1,10 @@
 ﻿
 
-function SyncAllActiveDirectory{
+function SyncAllActiveDirectory {
+
 
 # replicate all domain controllers
-(Get-ADDomainController -Filter *).Name | Foreach-Object {repadmin /syncall $_ (Get-ADDomain).DistinguishedName /e /A | Out-Null}; Start-Sleep 10; Get-ADReplicationPartnerMetadata -Target "$env:userdnsdomain" -Scope Domain | Select-Object Server, LastReplicationSuccess)
+(Get-ADDomainController -Filter *).Name | Foreach-Object {repadmin /syncall $_ (Get-ADDomain).DistinguishedName /e /A | Out-Null}; Start-Sleep 10; Get-ADReplicationPartnerMetadata -Target "$env:userdnsdomain" -Scope Domain | Select-Object Server, LastReplicationSuccess
 
 
 
