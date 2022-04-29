81decde027d6efbf9a9af60ff2223c102b469ea2
jbarlocker <jake@barlocker.org>
2022-04-29 10:53:07 -0600

    Add files via upload

--- MainMenu.ps1
+++ MainMenu.ps1
@@ -23,7 +23,7 @@ Function Master-Menu{
 Clear-Host
 $MenuTitle = @"
  
-Powershell Menu
+Powershell Menu for CC_Utah
 
 by: Jake Barlocker
 
@@ -36,16 +36,16 @@ Function ListMenu{
          Clear-Host
          Write-Host $MenuTitle -ForegroundColor Magenta
          Write-Host ""
-         Write-Host "==================== Please select an option ====================="
+         Write-Host "===================================================================="
          Write-Host ""
-         Write-Host "Press '1' to Choice1      - Shadow RDP into a computer"
-         Write-Host "Press '2' to Choice2      - Get Windows Updates on a remote computer"
-         Write-Host "Press '3' to Choice3      - Create a new user"
-         Write-Host "Press '4' to Choice4      - Delete a user"
-         Write-Host "Press '5' to Choice5      - Choice5"
-         Write-Host "Press '6' to Choice6      - Choice6"
-         Write-Host "Press '7' to Choice7      - Choice7"
-         Write-Host "Press 'Q' to Quit"
+         Write-Host "Enter '1' to Choice1      - Shadow RDP into a computer"
+         Write-Host "Enter '2' to Choice2      - Get Windows Updates on a remote computer"
+         Write-Host "Enter '3' to Choice3      - Create a new user"
+         Write-Host "Enter '4' to Choice4      - Delete a user"
+         Write-Host "Enter '5' to Choice5      - Choice5"
+         Write-Host "Enter '6' to Choice6      - Choice6"
+         Write-Host "Enter '7' to Choice7      - Choice7"
+         Write-Host "Enter 'Q' to Quit"
          Write-Host ""
          $MenuChoice = Read-Host "Select an option"
      }
@@ -55,16 +55,16 @@ Function ListMenu{
     $Global:Confirm=$False
 
     if($MenuChoice -match 1){
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="BOILER";$repo="PowershellScripts"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/DellProSupportGse/Tools/main/BOILER.ps1'));Invoke-BOILER
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="ShadowRDP";$repo="PowershellScripts"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/jbarlocker/PowershellMenu/main/ShadowRDP.ps1'));Start_Shadow_RDP
     }
     if($MenuChoice -match 2){
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="DART";$repo="PowershellScripts"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/DellProSupportGse/Tools/main/DART.ps1'));Invoke-DART
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="DART";$repo="PowershellScripts"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/jbarlocker/PowershellMenu/main/Choice2.ps1'));Invoke-Choice2function
     }
     if($MenuChoice -match 3){
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="FLEP";$repo="PowershellScripts"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/DellProSupportGse/Tools/main/FLEP.ps1'));Invoke-FLEP
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="FLEP";$repo="PowershellScripts"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/jbarlocker/PowershellMenu/main/Choice3.ps1'));Invoke-Choice3function
     }
     if($MenuChoice -match 4){
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="FLCkr";$repo="PowershellScripts"'+(new-object System.net.webclient).DownloadString('https://raw.githubusercontent.com/DellProSupportGse/Tools/main/FLCkr.ps1'));Invoke-FLCkr
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="FLCkr";$repo="PowershellScripts"'+(new-object System.net.webclient).DownloadString('https://raw.githubusercontent.com/jbarlocker/PowershellMenu/main/Choice4.ps1'));Invoke-Choice4function
     }
     if($MenuChoice -match 5){
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="LogCollector";$repo="PowershellScripts"'+(new-object System.net.webclient).DownloadString('https://raw.githubusercontent.com/DellProSupportGse/Tools/main/LogCollector.ps1'));Invoke-LogCollector
---
+++ ShadowRDP.ps1
@@ -0,0 +1,41 @@
+﻿#############################################################################################################################################
+####    
+####    Created by:  Jake Barlocker
+####    Created on:  3-NOV-2021
+####    
+####    This script will query and connect to a store computer via RDP Shadowing.
+####    
+####    http://woshub.com/rds-shadow-how-to-connect-to-a-user-session-in-windows-server-2012-r2/
+####    http://woshub.com/rdp-session-shadow-to-windows-10-user/
+####    https://thesysadminchannel.com/how-to-enable-remote-desktop-via-group-policy-gpo/
+####    
+#############################################################################################################################################
+
+
+
+Function Start_Shadow_RDP{
+Clear
+Write-Host ""
+Write-Host ""
+Write-Host "This script must be run on a computer in the checkcity.local domain." -ForegroundColor Red
+Write-Host ""
+Write-Host ""
+$ShadowTarget = Read-Host "What workstation do you want to shadow?  (example: u210w6.checkcity.local) "
+
+
+
+$TargetSessions = query session /server:$ShadowTarget
+Write-Host ""
+$TargetSessions
+Write-Host ""
+Write-Host ""
+$SessionToShadow = Read-Host "Enter the ID of the session you want to shadow"
+
+If (!$SessionToShadow) {} else {
+                        mstsc /v:$ShadowTarget /shadow:$SessionToShadow /NoConsentPrompt
+                        }
+
+
+
+                            }
+
