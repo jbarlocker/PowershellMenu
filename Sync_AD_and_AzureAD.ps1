1b12859771315e53406fbcc99309a8a19c9e7488
jbarlocker <jake@barlocker.org>
2022-04-29 11:15:43 -0600

    Add files via upload

--- Sync_AD_and_AzureAD.ps1
+++ Sync_AD_and_AzureAD.ps1
@@ -2,11 +2,15 @@
 
 function SyncAllActiveDirectory {
 
+Write-Host "Initiating Synchronizations on Domain Controllers..." -ForegroundColor Green
 
 # replicate all domain controllers
 (Get-ADDomainController -Filter *).Name | Foreach-Object {repadmin /syncall $_ (Get-ADDomain).DistinguishedName /e /A | Out-Null}; Start-Sleep 10; Get-ADReplicationPartnerMetadata -Target "$env:userdnsdomain" -Scope Domain | Select-Object Server, LastReplicationSuccess
 
 
+
+Write-Host ""
+Write-Host "Initiating Synchronizations to Azure Active Directory..." -ForegroundColor Green
 
 #Import modeult to sunc to Azure AD
 Import-Module ADSync
@@ -16,6 +20,8 @@ Start-ADSyncSyncCycle -PolicyType Delta
 
 # replicate to Azure AD (full sync)
 #Start-ADSyncSyncCycle -PolicyType Initial
+
+
 
 
                                 }
