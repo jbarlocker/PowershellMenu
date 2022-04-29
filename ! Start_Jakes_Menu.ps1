eb4efa47ab18a46d0711245a25d6785438e3ee42
jbarlocker <jake@barlocker.org>
2022-04-29 10:56:23 -0600

    Add files via upload

---
+++ ! Start_Jakes_Menu.ps1
@@ -0,0 +1,11 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="ListMenu";$repo="PowershellMenu"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/jbarlocker/PowershellMenu/main/MainMenu.ps1'));Master-Menu 
+
+
+
+
+
+
+#Echo ToolBox
+#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#Invoke-Expression('$module="ToolBox";$repo="PowershellScripts"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/DellProSupportGse/Tools/main/ToolBox.ps1'))
+#Invoke-ToolBox 
