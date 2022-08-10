db9ba2ffc3313df18b7f4bd97c01221d479c174f
jbarlocker <jake@barlocker.org>
2022-08-10 08:33:53 -0600

    Update CC_MainMenu.ps1

--- CC_MainMenu.ps1
+++ CC_MainMenu.ps1
@@ -1,4 +1,4 @@
-﻿######################################################################
+######################################################################
 ###   
 ###   Created By: Jake Barlocker
 ###   Created on:  29-APR-2022
@@ -50,8 +50,9 @@ Function ListMenu{
          Write-Host ""
          Write-Host "Enter '1' - Shadow RDP into a computer"
          Write-Host "Enter '2' - Sync all domain controllers, then sync to AzureAD"
-         Write-Host "Enter '3' - Reset the Local Admin Password on all domain joined computers"
-         Write-Host "Enter '4' - Scan domain servers for expiring certificates"
+         Write-Host "Enter '3' - Create new user in the checkcity.local domain"
+         Write-Host "Enter '4' - Reset the Local Admin Password on all domain joined computers"
+         Write-Host "Enter '5' - Scan domain servers for expiring certificates"
          #Write-Host "Enter '5' - Choice5"
          #Write-Host "Enter '6' - Choice6"
          #Write-Host "Enter '7' - Choice7"
@@ -71,9 +72,12 @@ Function ListMenu{
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="Sync_AD_and_AzureAD";$repo="PowershellScripts"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/jbarlocker/PowershellMenu/main/Sync_AD_and_AzureAD.ps1'));SyncAllActiveDirectory
     }
     if($MenuChoice -match 3){
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="Reset_Local_Admin_Passwords";$repo="PowershellScripts"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/jbarlocker/PowershellMenu/main/Reset_Local_Admin_Passwords.ps1'));ResetLocalAdminPassword
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="Create_New_AD_User";$repo="PowershellScripts"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/jbarlocker/PowershellMenu/main/Create_New_AD_User.ps1'));Create_New_AD_User
     }
     if($MenuChoice -match 4){
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="Reset_Local_Admin_Passwords";$repo="PowershellScripts"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/jbarlocker/PowershellMenu/main/Reset_Local_Admin_Passwords.ps1'));ResetLocalAdminPassword
+    }
+    if($MenuChoice -match 5){
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-Expression('$module="Scan_Domain_Servers_for_Expiring_Certificates";$repo="PowershellScripts"'+(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/jbarlocker/PowershellMenu/main/Scan_Domain_Servers_for_Expiring_Certificates.ps1'));Scan_for_Certs
     }
 <#    if($MenuChoice -match 5){
